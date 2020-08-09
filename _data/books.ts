ab36090d60e70b17395137e50156fac3233fad6a
Matt Zuzolo <mattzuzolo@outlook.com>
2020-08-09 10:43:53 -0400

    Add absolute imports to Gatsby config

---
+++ _data/books.ts
@@ -0,0 +1,16 @@
+import Book from '_types/Book';
+type Data = {};
+
+const data = {
+  '2020': [
+    {
+      title: 'The Flamethrowers',
+      slug: 'the-flamethrowers',
+      author: 'Rachel Kushner',
+      image: '',
+      favorite: true,
+      reread: false,
+    },
+  ],
+  '2019': [],
+};
---
+++ _types/book.ts
@@ -0,0 +1,8 @@
+export default type Book = {
+  title: string;
+  slug: string;
+  author: string;
+  image: string;
+  favorite: boolean;
+  reread: boolean;
+};
