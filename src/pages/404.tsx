a54ec32b843c6cfbe81c3510d7c643f57b4aba8f
Matt Zuzolo <mattzuzolo@outlook.com>
2020-08-11 23:20:52 -0400

    Add placeholder 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Layout from '../components/Layout';
+import Feed from '../components/FeedItem';
+
+const title = styled.h1`
+  color: red;
+  font-size: 42px;
+`;
+
+export default function Home(): JSX.Element {
+  return <title>404</title>;
+}
